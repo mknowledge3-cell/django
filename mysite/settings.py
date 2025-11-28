2ad1750009d6af4f0f2dacd4762da06bfbf324bb
M4Mi4 <m4mi4@outlook.com>
2025-11-28 16:17:11 +0200

    Contact page. Fix Brevo 3. Final product. v1.0.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk')
 
-DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
+# DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
+
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
